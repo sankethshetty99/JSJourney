0d6ca998ab2204a176c9f9b8c71fc26807d589a7
sankethsshetty <sankethshetty007@gmail.com>
2021-07-18 14:01:18 -0500

    macbook

---
+++ functioncomposition.js
@@ -0,0 +1,31 @@
+// instead of prototypical inheritance. 
+// this uses closures.
+// takes advantage of the scoping variations
+
+
+function Animal(name){
+
+    const howl = () => {
+        console.log("")
+    }
+
+    return {
+        counter++;
+
+    }
+}
+
+function Dog(name){
+
+}
+
+
+const chubbu = Dog('chubbu')
+const scooby = Dog('scooby')
+
+chubbu
+
+
+
+
+
