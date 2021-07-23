1aca484a52cc49c3593c587cee45e674ae4f9852
sankethsshetty <sankethshetty007@gmail.com>
2021-07-22 21:59:36 -0500

    more promise

--- app.js
+++ app.js
@@ -1,6 +1,2 @@
-'use strict';
-const pino = require('pino');
-const format = require('./format');
-const logger = pino();
-logger.info(format.upper('my-package started'));
-process.stdin.resume();
+
+hona hain !
